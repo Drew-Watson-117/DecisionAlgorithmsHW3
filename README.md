f3e13e8dd829b3fa00772a1a78e693ee46e6c0e6
Andrew Watson <drewzidrew1@gmail.com>
2023-02-21 17:25:14 -0700

    Finished?

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Part 1
 
-- Idea: Refactor the `europeanMonteCarlo.py` to use 
+## Idea
+- Refactor the `europeanMonteCarlo.py` to use 
 `np.beta(14,6) - 0.65` instead of the Brownian Motion.
 - Instead of 100 paths, do 5000.
 - Change Volatility and drift
 
+## Results
+
+- The ideal price of the option is approximately $904 (for a block of 100 options). 
 
 # Part 2
 
@@ -14,4 +18,23 @@
 - For stock 2, an alpha distribution fit best, with a sum square error of 0.022781
     - Best 5: alpha, genhyperbolic, chi2, invgamma, skewnorm
 - From the common distributions, the lognormal distribution fits both datasets best, with a sum square error of 0.029207 and 0.022877 respectively
-- For simplicity, we will use the lognormal distribution as the fit distribution
+- For simplicity, we will use the lognormal distribution as the fit distribution to calculate option value
+
+## Idea
+
+- Fit the stock data to a lognormal distribution
+    - Each stock gets its own distribution (2 total)
+- Pull down 365 values from each distribution as the estimated prices for each stock
+- For outperforming the average of the two stocks:
+    - If the value of the option at expiry is greater than the average value of the two stocks, then the payoff is option_price - average(stocks_prices)
+    - Else, the payoff of the option is 0
+- For outperforming the max of the two stocks:
+    - If the value of the option at expiry is greater than the max value of the two stocks, then the payoff is option_price - max(stocks_prices)
+    - Else, the payoff of the option is 0
+
+## Results
+
+- For outperforming the average value of the two stocks, the optimal price for the option is approximately $770 (for a block of 100 options)
+
+- The option will (almost) never outperform the max value of the two stocks, so the optimal price of the option is not to sell it ($0)
+    - This conclusion likely comes from the imperfection of my distribution model (i.e. not approximating the tails correctly). 
--- stock_simulations.py
+++ stock_simulations.py
@@ -18,21 +18,17 @@ class European_Call_Payoff:
 class Comparative_Payoff:
 
     def __init__(self, payoff1, payoff2):
-        self.payoff1 = payoff1
-        self.payoff2 = payoff2
-        
+        self.values = np.union1d(payoff1,payoff2)
 
     def get_payoff_average(self, stock_price):
-        allValues = np.union1d(self.payoff1, self.payoff2)
-        comparisonValue = np.average(allValues)
+        comparisonValue = np.average(self.values)
         if stock_price > comparisonValue:
             return stock_price - comparisonValue
         else:
             return 0
 
     def get_payoff_max(self, stock_price):
-        allValues = np.union1d(self.payoff1, self.payoff2)
-        comparisonValue = np.max(allValues)
+        comparisonValue = np.max(self.values)
         if stock_price > comparisonValue:
             return stock_price - comparisonValue
         else:
@@ -59,46 +55,6 @@ class BetaMotion:
         self.T = T
         self.prices = []
         self.simulate_paths()
-
-class FMotion:
-
-    def simulate_paths(self, dfn, dfd, loc, scale):
-        while(self.T - self.dt > 0):
-            dWt = scale*np.random.f(dfn,dfd) - loc # Lognormal Motion
-            dYt = self.drift*self.dt + self.volatility*dWt  # Change in price
-            self.current_price += dYt  # Add the change to the current price
-            self.prices.append(self.current_price)  # Append new price to series
-            self.T -= self.dt  # Account for the step in time
-
-    def __init__(self, initial_price, drift, volatility, dt, T, dfn, dfd, loc, scale):
-        self.current_price = initial_price
-        self.initial_price = initial_price
-        self.drift = drift
-        self.volatility = volatility
-        self.dt = dt
-        self.T = T
-        self.prices = []
-        self.simulate_paths(dfn, dfd, loc, scale)
-
-class LognormalMotion:
-
-    def simulate_paths(self, sigma, loc, scale):
-        while(self.T - self.dt > 0):
-            dWt = scale * np.random.lognormal(sigma=sigma) - loc # Lognormal Motion
-            dYt = self.drift*self.dt + self.volatility*dWt  # Change in price
-            self.current_price += dYt  # Add the change to the current price
-            self.prices.append(self.current_price)  # Append new price to series
-            self.T -= self.dt  # Account for the step in time
-
-    def __init__(self, initial_price, drift, volatility, dt, T, sigma, loc, scale):
-        self.current_price = initial_price
-        self.initial_price = initial_price
-        self.drift = drift
-        self.volatility = volatility
-        self.dt = dt
-        self.T = T
-        self.prices = []
-        self.simulate_paths(sigma, loc, scale)
 
 def fitStock(fileName):
     stock = pd.read_csv(fileName, names=["Data"])
